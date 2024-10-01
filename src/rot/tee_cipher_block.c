a31c520dda329ea503624ba07086f1a4148e0aa1
Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
2024-10-01 12:03:57 +0200

    Use device root key for sealed key encryption

--- src/rot/tee_cipher_block.c
+++ src/rot/tee_cipher_block.c
@@ -11,37 +11,14 @@
 
 int cc310_aes_init(cipher_context_t *context, const uint8_t *key, uint8_t keySize)
 {
-    uint8_t i;
-
-    if (keySize != AES_KEY_SIZE_128) {
-        return CIPHER_ERR_INVALID_KEY_SIZE;
-    }
-
-    context->key_size = keySize;
-
-    /* Make sure that context is large enough. If this is not the case,
-       you should build with -DAES */
-    if (CIPHER_MAX_CONTEXT_SIZE < keySize) {
-        return CIPHER_ERR_BAD_CONTEXT_SIZE;
-    }
-
-    /* key must be at least CIPHERS_MAX_KEY_SIZE Bytes long */
-    if (keySize < CIPHERS_MAX_KEY_SIZE) {
-        /* fill up by concatenating key to as long as needed */
-        for (i = 0; i < CIPHERS_MAX_KEY_SIZE; i++) {
-            context->context[i] = key[(i % keySize)];
-        }
-    }
-    else {
-        for (i = 0; i < CIPHERS_MAX_KEY_SIZE; i++) {
-            context->context[i] = key[i];
-        }
-    }
+    (void) context;
+    (void) key;
+    (void) keySize;
 
     return CIPHER_INIT_SUCCESS;
 }
 
-static int cc310_aes_encrypt_decrypt_with_root_key(const uint8_t *input, size_t input_len,
+static CYS_error_t cc310_aes_128_encrypt_decrypt_with_root_key(uint8_t *input, size_t input_len,
                 uint8_t *output, size_t output_len, int mode)
 {
     /* Enable CRYPTOCELL subsystem */
@@ -55,9 +32,6 @@ static int cc310_aes_encrypt_decrypt_with_root_key(const uint8_t *input, size_t 
 
     /* Wait until crypto engine is Idle  */
     while (TEE_CC_CTL->CRYPTO_BUSY == TEE_CC_CTL_CRYPTO_BUSY_STATUS_Busy) { }
-
-    TEE_CC_DOUT->DOUT_SW_RESET = 1;
-    TEE_CC_AES->AES_SW_RESET = 1;
 
     /* Select HW Key */
     TEE_CC_HOST_RGF->HOST_CRYPTOKEY_SEL = 0;
@@ -77,15 +51,18 @@ static int cc310_aes_encrypt_decrypt_with_root_key(const uint8_t *input, size_t 
     /* Wait on DOUT DMA interrupt */
     while(!(TEE_CC_HOST_RGF->IRR & TEE_CC_HOST_RGF_IRR_DOUT_TO_MEM_INT_Msk)) { }
 
+    TEE_CC_DOUT->DOUT_SW_RESET = 1;
     TEE_CC_AES->AES_SW_RESET = 1;
     TEE_CRYPTOCELL->ENABLE = ~CRYPTOCELL_ENABLE_ENABLE_Enabled;
+
+    return CYS_SUCCESS;
 }
 
 int cc310_aes_encrypt_block(const cipher_context_t *context, const uint8_t *plain_block,
                 uint8_t *cipher_block)
 {
-    CYS_error_t ret = cc310_aes_128_encrypt_decrypt((uint32_t *)context->context, NULL, (uint8_t *)plain_block, AES_BLOCK_SIZE, cipher_block, AES_BLOCK_SIZE, TEE_CC_AES_ECB_ENCRYPT);
-
+    (void) context;
+    CYS_error_t ret = cc310_aes_128_encrypt_decrypt_with_root_key((uint8_t *)plain_block, AES_BLOCK_SIZE, cipher_block, AES_BLOCK_SIZE, TEE_CC_AES_ECB_ENCRYPT);
     if (ret != CYS_SUCCESS) {
         return CIPHER_ERR_ENC_FAILED;
     }
@@ -96,7 +73,8 @@ int cc310_aes_encrypt_block(const cipher_context_t *context, const uint8_t *plai
 int cc310_aes_decrypt_block(const cipher_context_t *context, const uint8_t *cipher_block,
                 uint8_t *plain_block)
 {
-    CYS_error_t ret = cc310_aes_128_encrypt_decrypt((uint32_t *)context->context, NULL, (uint8_t *)cipher_block, AES_BLOCK_SIZE, plain_block, AES_BLOCK_SIZE, TEE_CC_AES_ECB_DECRYPT);
+    (void) context;
+    CYS_error_t ret = cc310_aes_128_encrypt_decrypt_with_root_key((uint8_t *)cipher_block, AES_BLOCK_SIZE, plain_block, AES_BLOCK_SIZE, TEE_CC_AES_ECB_DECRYPT);
 
     if (ret != CYS_SUCCESS) {
         return CIPHER_ERR_DEC_FAILED;
--- src/rot/tee_rot.c
+++ src/rot/tee_rot.c
@@ -1,6 +1,3 @@
-#include "nrf9160.h"
-#include "nrf9120_bitfields.h"
-
 #include "ocb.h"
 #include "random.h"
 #include "CYS/common.h"
@@ -8,73 +5,29 @@
 #include "cc310_driver/cc310_registers.h"
 
 #include "tee_rot.h"
-// #include "cc310_driver/cc310_aes_128.c"
 
 #define TEE_ROT_KEY_AES_ID          (3)
 #define TEE_ROT_AES_128_KEY_BYTES   (16)
 
-static const AES_TEST_KEY[TEE_ROT_AES_128_KEY_BYTES] = {
+/* This is for testing only! Ideally key should be set by an immutable bootloader or at least randomly generated! */
+static const uint8_t AES_TEST_KEY[TEE_ROT_AES_128_KEY_BYTES] = {
     0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
     0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c
 };
 
-/**
- * @brief   Permissions for AES key.
- *
- *          Bit 0 [Write]   : 0     Not allowed
- *          Bit 1 [Read]    : 1     Allowed
- *          Bit 2 [Push]    : 0     Disabled
- *          Bit 16[State]   : 1     Active
- *
- * @warning These are insecure, because the CPU can read the key from the UICR.
- *          This is a placeholder, until there is a Cryptocell driver implementation available.
- */
-#define TEE_ROT_KEY_AES_PERM           (0xFFFFFFFA)
-
 CYS_error_t tee_rot_try_generate_aes_key(void)
 {
-    /* Select key slot 3 */
-    // NRF_KMU_S->SELECTKEYSLOT = TEE_ROT_KEY_AES_ID;
-
-    /* Index N in UICR->KEYSLOT.KEY[N] and UICR->KEYSLOT.CONFIG[N]
-    corresponds to KMU key slot ID=N+1. */
-    // if (NRF_UICR_S->KEYSLOT.CONFIG[TEE_ROT_KEY_AES_ID-1].PERM != 0xFFFFFFFF) {
-    //     /* If KEYSLOT.CONFIG != 0xFFFFFFFF, this means this slot has already been configured */
-    //     NRF_KMU_S->SELECTKEYSLOT = 0;
-    //     return CYS_ERROR_ALREADY_EXISTS;
-    // }
-
-    // /* Enable writes to flash */
-    // NRF_NVMC_S->CONFIG = NVMC_CONFIG_WEN_Wen;
-
-    /* Generate AES key */
-    // uint32_t aes_key[TEE_ROT_AES_128_KEY_BYTES/4];
-    // random_bytes((uint8_t *)aes_key, TEE_ROT_AES_128_KEY_BYTES);
-
-    /* Write AES key to hardware key slot */
-    // NRF_UICR_S->KEYSLOT.KEY[TEE_ROT_KEY_AES_ID-1].VALUE[0] = aes_key[0];
-    // NRF_UICR_S->KEYSLOT.KEY[TEE_ROT_KEY_AES_ID-1].VALUE[1] = aes_key[1];
-    // NRF_UICR_S->KEYSLOT.KEY[TEE_ROT_KEY_AES_ID-1].VALUE[2] = aes_key[2];
-    // NRF_UICR_S->KEYSLOT.KEY[TEE_ROT_KEY_AES_ID-1].VALUE[3] = aes_key[3];
-    if (TEE_CC_HOST_RGF->KDR0) {
+    if (TEE_CC_HOST_RGF->HOST_IOT_KDR0) {
         return CYS_ERROR_ALREADY_EXISTS;
     }
 
-    TEE_CC_HOST_RGF->KDR0 = AES_TEST_KEY[0];
-    TEE_CC_HOST_RGF->KDR1 = AES_TEST_KEY[1];
-    TEE_CC_HOST_RGF->KDR2 = AES_TEST_KEY[2];
-    TEE_CC_HOST_RGF->KDR3 = AES_TEST_KEY[3];
+    /* This is for testing only! Ideally key should be set by an immutable bootloader or at least randomly generated! */
+    TEE_CC_HOST_RGF->HOST_IOT_KDR0 = ((uint32_t *)AES_TEST_KEY)[0];
+    TEE_CC_HOST_RGF->HOST_IOT_KDR1 = ((uint32_t *)AES_TEST_KEY)[1];
+    TEE_CC_HOST_RGF->HOST_IOT_KDR2 = ((uint32_t *)AES_TEST_KEY)[2];
+    TEE_CC_HOST_RGF->HOST_IOT_KDR3 = ((uint32_t *)AES_TEST_KEY)[3];
 
-    /* Configure key permissions */
-    // NRF_UICR_S->KEYSLOT.CONFIG[TEE_ROT_KEY_AES_ID-1].PERM = TEE_ROT_KEY_AES_PERM;
-
-    /* Disable writes to flash */
-    // NRF_NVMC_S->CONFIG = NVMC_CONFIG_WEN_Ren;
-
-    /* Deselect key slot 3 */
-    // NRF_KMU_S->SELECTKEYSLOT = 0;
-
-    if (TEE_CC_HOST_RGF->KDR0) {
+    if (TEE_CC_HOST_RGF->HOST_IOT_KDR0) {
         return CYS_SUCCESS;
     }
 
@@ -83,29 +36,10 @@ CYS_error_t tee_rot_try_generate_aes_key(void)
 
 CYS_error_t tee_rot_encrypt_key_ocb(uint8_t *key_in, CYS_PROT_ecc_p256_key_t *sealed_key)
 {
-    /* Select key slot 3 */
-    // NRF_KMU_S->SELECTKEYSLOT = TEE_ROT_KEY_AES_ID;
+    cipher_t cipher = { 0 };
 
-    /* Check if permissions are set correctly */
-    // if (NRF_UICR_S->KEYSLOT.CONFIG[TEE_ROT_KEY_AES_ID-1].PERM != TEE_ROT_KEY_AES_PERM) {
-    //     NRF_KMU_S->SELECTKEYSLOT = 0;
-    //     return CYS_ERROR_GENERIC_ERROR;
-    // }
-
-    // /* This is unsecure and only temporary until cryptocell can be used directly */
-    // uint32_t aes_key[TEE_ROT_AES_128_KEY_BYTES/4];
-    // aes_key[0] = NRF_UICR_S->KEYSLOT.KEY[TEE_ROT_KEY_AES_ID-1].VALUE[0];
-    // aes_key[1] = NRF_UICR_S->KEYSLOT.KEY[TEE_ROT_KEY_AES_ID-1].VALUE[1];
-    // aes_key[2] = NRF_UICR_S->KEYSLOT.KEY[TEE_ROT_KEY_AES_ID-1].VALUE[2];
-    // aes_key[3] = NRF_UICR_S->KEYSLOT.KEY[TEE_ROT_KEY_AES_ID-1].VALUE[3];
-
-    cipher_t cipher;
-    cipher_init(&cipher, CIPHER_AES, (uint8_t *)aes_key, TEE_ROT_AES_128_KEY_BYTES);
-
+    /* The hardware driver ignores the cipher context, so no need to initialize */
     int32_t result = cipher_encrypt_ocb(&cipher, NULL, 0, CYS_PROT_SEAL_TAG_SIZE, sealed_key->nonce, CYS_PROT_SEAL_NONCE_SIZE, key_in, CYS_PROT_ECC_P256_KEY_SIZE, sealed_key->private_key);
-
-    /* Deselect key slot 3 */
-    // NRF_KMU_S->SELECTKEYSLOT = 0;
 
     if(result == CYS_PROT_ECC_P256_KEY_SIZE + CYS_PROT_SEAL_TAG_SIZE) {
         return CYS_SUCCESS;
@@ -116,29 +50,10 @@ CYS_error_t tee_rot_encrypt_key_ocb(uint8_t *key_in, CYS_PROT_ecc_p256_key_t *se
 
 CYS_error_t tee_rot_decrypt_key_ocb(CYS_PROT_ecc_p256_key_t *sealed_key, uint8_t *key_out)
 {
-    // /* Select key slot 3 */
-    // NRF_KMU_S->SELECTKEYSLOT = TEE_ROT_KEY_AES_ID;
+    cipher_t cipher = { 0 };
 
-    // /* Check if permissions are set correctly */
-    // if (NRF_UICR_S->KEYSLOT.CONFIG[TEE_ROT_KEY_AES_ID-1].PERM != TEE_ROT_KEY_AES_PERM) {
-    //     NRF_KMU_S->SELECTKEYSLOT = 0;
-    //     return CYS_ERROR_NOT_PERMITTED;
-    // }
-
-    // /* This is unsecure and only temporary until cryptocell can be used directly */
-    // uint32_t aes_key[TEE_ROT_AES_128_KEY_BYTES/4];
-    // aes_key[0] = NRF_UICR_S->KEYSLOT.KEY[TEE_ROT_KEY_AES_ID-1].VALUE[0];
-    // aes_key[1] = NRF_UICR_S->KEYSLOT.KEY[TEE_ROT_KEY_AES_ID-1].VALUE[1];
-    // aes_key[2] = NRF_UICR_S->KEYSLOT.KEY[TEE_ROT_KEY_AES_ID-1].VALUE[2];
-    // aes_key[3] = NRF_UICR_S->KEYSLOT.KEY[TEE_ROT_KEY_AES_ID-1].VALUE[3];
-
-    cipher_t cipher;
-    cipher_init(&cipher, CIPHER_AES, (uint8_t *)aes_key, TEE_ROT_AES_128_KEY_BYTES);
-
+    /* The hardware driver ignores the cipher context, so no need to initialize */
     int32_t result = cipher_decrypt_ocb(&cipher, NULL, 0, CYS_PROT_SEAL_TAG_SIZE, sealed_key->nonce, CYS_PROT_SEAL_NONCE_SIZE, sealed_key->private_key, CYS_PROT_ECC_P256_KEY_SIZE+CYS_PROT_SEAL_TAG_SIZE, key_out);
-
-    /* Deselect key slot 3 */
-    // NRF_KMU_S->SELECTKEYSLOT = 0;
 
     return CYS_SUCCESS;
 }
